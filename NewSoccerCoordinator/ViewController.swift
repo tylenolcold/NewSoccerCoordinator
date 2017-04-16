a3d9dc6eb99cc6f6a6faa7d6fdb392afb21f8290
James Lee <jameslee@JamesdeMac.local>
2017-04-16 22:47:22 +0800

    Initial Commit

---
+++ NewSoccerCoordinator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewSoccerCoordinator
+//
+//  Created by James Lee on 2017/4/16.
+//  Copyright © 2017年 James Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
